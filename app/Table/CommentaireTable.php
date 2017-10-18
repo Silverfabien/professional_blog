adc029c1fd186d0ba883b2c35a3b1c2c84ff1b3f
Hollebeque Fabien <hollebeque.fabien@hotmail.com>
2017-10-18 16:58:36 +0200

    add comment

---
+++ app/Table/CommentaireTable.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Table;
+
+use Core\Table\Table;
+
+class CommentaireTable extends Table
+{
+	protected $table = 'commentaire';
+
+	/**
+	 * Récupère les derniers article
+	 * @return array
+	 */
+
+	public function last()
+	{
+		return $this->query("SELECT commentaire.id, commentaire.contenu, commentaire.auteur, commentaire.date FROM commentaire ORDER BY commentaire.date DESC");
+	}
+
+	public function lastCommentaire($id)
+	{
+		return $this->query("SELECT commentaire.id, commentaire.auteur, commentaire.contenu, commentaire.date FROM commentaire WHERE commentaire.id = ?", [$id], true);
+	}
+}
+
+?>
