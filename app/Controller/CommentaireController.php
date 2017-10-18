4bd6d06211fd4231d4f8babc99a433c0ac784ada
Hollebeque Fabien <hollebeque.fabien@hotmail.com>
2017-10-18 16:57:42 +0200

    add comment

---
+++ app/Controller/CommentaireController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Controller;
+
+use Core\Controller\Controller;
+use Core\HTML\BootstrapForm;
+
+class CommentaireController extends AppController
+{
+	public function __construct()
+	{
+		parent::__construct();
+		$this->loadModel('Commentaire');
+	}
+
+	public function index()
+	{
+		$commentaires = $this->Commentaire->last();
+		$this->render('commentaire.index', compact('commentaires'));
+	}
+
+	public function show()
+	{
+		$com = $this->Commentaire->lastCommentaire($_GET['id']);
+		$this->render('commentaire.show', compact('com'));
+	}
+
+	public function add()
+	{
+		if(!empty($_POST))
+		{
+			$result = $this->Commentaire->create(['auteur' => $_POST['auteur'], 'contenu' => $_POST['contenu']]);
+			if($result)
+			{
+				return $this->index();
+			}
+		}
+		$form = new BootstrapForm($_POST);
+		$this->render('commentaire.edit', compact('form'));
+	}
+
+	public function edit()
+	{
+		if(!empty($_POST))
+		{
+			$result = $this->Commentaire->update($_GET['id'], ['auteur' => $_POST['auteur'], 'contenu' => $_POST['contenu']]);
+			if($result)
+			{
+				return $this->index();
+			}
+		}
+		$commentaire = $this->Commentaire->find($_GET['id']);
+		$form = new BootstrapForm($commentaire);
+		$this->render('commentaire.edit', compact('form'));
+	}
+}
