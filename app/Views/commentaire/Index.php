8fe8633bc1c7753c1c3769d19431f9056f1c03a9
Hollebeque Fabien <hollebeque.fabien@hotmail.com>
2017-10-18 16:59:39 +0200

    add comment

---
+++ app/Views/commentaire/Index.php
@@ -0,0 +1,15 @@
+<p>
+	<a href="?p=commentaire.add" class="btn btn-success">Ajouter</a>
+</p>
+<div class="row">
+	<div class="col-sm-8">
+
+		<?php foreach($commentaires as $commentaire): ?>
+
+			<h2><a href="<?= $commentaire->url ?>">a</a></h2>
+
+			<p><strong><?= htmlspecialchars($commentaire->auteur); ?></strong> a posté le post le <?= $commentaire->dateFormat; ?></p>
+
+			<p><?= htmlspecialchars($commentaire->contenu); ?></p>
+
+		<?php endforeach; ?>
---
+++ app/Views/commentaire/Show.php
@@ -0,0 +1,6 @@
+<h1><?= htmlspecialchars($com->auteur); ?></h1>
+
+<p><strong><em><?= htmlspecialchars($com->auteur); ?></strong> a posté le post le <?= $com->date; ?></em></p>
+
+<p><?= htmlspecialchars($com->contenu); ?></p>
+
