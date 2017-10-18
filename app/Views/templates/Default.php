045b43f9804c425dd97eaac49e598276e4aef06c
Hollebeque Fabien <hollebeque.fabien@hotmail.com>
2017-10-18 16:52:53 +0200

    Modification of the theme and addition of presentation

--- app/Views/templates/Default.php
+++ app/Views/templates/Default.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
+
   <head>
+
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
@@ -10,21 +12,49 @@
     <title><?= App::getInstance()->title; ?></title>
 
     <!-- Bootstrap core CSS -->
-    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/one-page-wonder.css" rel="stylesheet">
     <link rel="stylesheet" href="css/Blog.css">
+
   </head>
 
   <body>
 
-    <nav class="navbar navbar-inverse navbar-fixed-top">
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
       <div class="container">
-        <div class="navbar-header">
-          <a class="navbar-brand" href="index.php">Accueil</a>
-          <a class="navbar-brand" href="Blog.php">Blog</a>
+        <a class="navbar-brand" href="Index.php"><?= App::getInstance()->title; ?></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="Index.php">Accueil
+                <span class="sr-only">(current)</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="Blog.php">Blog</a>
+            </li>
+          </ul>
         </div>
       </div>
     </nav>
 
+    <header class="masthead">
+      <div class="overlay">
+        <div class="container">
+          <h1 class="display-1 text-white">Hollebeque Fabien</h1>
+          <h2 class="display-4 text-white">Phrase d'accroche</h2>
+        </div>
+      </div>
+    </header>
+
+        <p class="img-logo"><img class="img-logo-style img-fluid rounded-circle" src="images/logo.jpg"></p>
+    
     <div class="container">
 
       <div class="starter-template" style="padding-top: 100px">
@@ -32,6 +62,15 @@
       </div>
       
     </div><!-- /.container -->
+
+
+    <!-- Footer -->
+    <footer class="py-5 bg-dark">
+      <div class="container">
+        <p class="m-0 text-center text-white">Copyright &copy; Your Website 2017</p>
+      </div>
+      <!-- /.container -->
+    </footer>
 
   </body>
 </html>
