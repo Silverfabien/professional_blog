c11a93eba45a38df5e7c7b70b366367452040df6
Hollebeque Fabien <hollebeque.fabien@hotmail.com>
2017-10-19 14:11:33 +0200

    adding the 'éditer' button

--- app/Views/commentaire/Show.php
+++ app/Views/commentaire/Show.php
@@ -1,6 +1,10 @@
+<a class="btn btn-success" href="?p=commentaire.edit&id=<?= $com->id; ?>">Editer</a>
+
 <h1><?= htmlspecialchars($com->auteur); ?></h1>
 
-<p><strong><em><?= htmlspecialchars($com->auteur); ?></strong> a posté le post le <?= $com->date; ?></em></p>
+<p><strong><em><?= htmlspecialchars($com->auteur); ?></strong> a posté le post le <?= $com->dateFormat; ?></em></p>
 
 <p><?= htmlspecialchars($com->contenu); ?></p>
+
+<a href="Blog.php?p=commentaire.index">Index</a>
 
