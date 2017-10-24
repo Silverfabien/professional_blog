6c70b789eefc09009e77bb7a3b68c7d226cacd5c
Hollebeque Fabien <hollebeque.fabien@hotmail.com>
2017-10-24 17:53:39 +0200

    deleted

--- app/Views/posts/Commentaire.php
+++
@@ -1,8 +0,0 @@
-<?php var_dump($commentaire); ?>
-
-<h1><?= htmlspecialchars($commentaire->auteur); ?></h1>
-
-<p><strong><em><?= htmlspecialchars($commentaire->auteur); ?></strong> a posté le post le <?= $commentaire->date; ?></em></p>
-
-<p><?= htmlspecialchars($commentaire->contenu); ?></p>
-
