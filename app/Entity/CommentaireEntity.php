99e9d7d02d43cc4e805fcf0b5379e261b9b54c8a
Hollebeque Fabien <hollebeque.fabien@hotmail.com>
2017-10-29 14:37:20 +0100

    location of modification

--- app/Entity/CommentaireEntity.php
+++ app/Entity/CommentaireEntity.php
@@ -8,14 +8,14 @@ class CommentaireEntity extends Entity
 {
 	public function getUrl()
 	{
-		return 'Blog.php?p=commentaire.show&id=' . $this->id;
+		return 'Index.php?p=commentaire.show&id=' . $this->id;
 	}
 
 	public function getDateformat()
 	{
 		$a = strtotime($this->date);
 		setlocale(LC_ALL, 'FR').': ';
-		$date = iconv('ISO-8859-1', 'UTF-8', strftime('%A %d %B %Y', $a) . ' a ' . strftime('%H', $a) . 'h' . strftime('%M', $a)) .'<br />';
+		$date = iconv('ISO-8859-1', 'UTF-8', strftime('%A %d %B %Y', $a) . ' a ' . strftime('%H', $a) . 'h' . strftime('%M', $a));
 		return $date;
 	}
 }
