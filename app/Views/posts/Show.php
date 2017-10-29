478e5f52029692f4f1fe2d7a6fb9040efa85c2c6
Hollebeque Fabien <hollebeque.fabien@hotmail.com>
2017-10-29 14:55:53 +0100

    add of css and commentaries

--- app/Views/posts/Show.php
+++ app/Views/posts/Show.php
@@ -1,12 +1,22 @@
 <a class="btn btn-primary" href="?p=posts.edit&id=<?= $article->id; ?>">Editer</a>
 <a class="btn btn-success" href="?p=commentaire.add&id=<?= $article->id; ?>">Commenter</a>
+<a href="Index.php">Index</a>
 
-<h1><?= htmlspecialchars($article->titre); ?></h1>
+<div style="background: #eeeeee; border: 2px solid #bbbbbb;">
+	<div style="background: #dddddd; border-bottom: 1px solid #bbbbbb;">
+		<h1><?= htmlspecialchars($article->titre); ?></h1>
 
-<p><strong><em><?= htmlspecialchars($article->auteur); ?></strong> a posté le post le <?= $article->dateFormat; ?></em></p>
+		<p><strong><em><?= htmlspecialchars($article->auteur); ?></strong> a posté le post le <?= $article->dateFormat; ?></em></p>
+	</div>
 
-<p><?= htmlspecialchars($article->contenu); ?></p>
+	<p><?= htmlspecialchars($article->contenu); ?></p>
+</div><br>
 
-<a href="Blog.php">Index</a>
+<?php foreach ($commentaires as $commentaire): ?>
+	
+	<h3><?= htmlspecialchars($commentaire->auteur); ?></h3>
+	<p><em><?= htmlspecialchars($commentaire->dateFormat); ?></em></p>
+	<p><?= htmlspecialchars($commentaire->contenu); ?></p>
+	<a href="?p=commentaire.edit&id=<?= $commentaire->id; ?>">Éditer</a>
 
-<p><?= htmlspecialchars($commentaires->contenu); ?></p>
+<?php endforeach; ?>
