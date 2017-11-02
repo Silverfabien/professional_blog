bcb0b590e77e08df0d7e4b23a12dc24afe036224
Hollebeque Fabien <hollebeque.fabien@hotmail.com>
2017-11-02 13:19:59 +0100

    No change

--- app/Views/commentaire/Edit.php
+++ app/Views/commentaire/Edit.php
@@ -1,5 +1,6 @@
 <form method="post">
-	<?= $form->input('auteur', 'nom l\'auteur'); ?>
-	<?= $form->input('contenu', 'contenu', ['type' => 'textarea']); ?>
+	<?= $form->input('auteur', 'nom l\'auteur :'); ?>
+	<?= $form->input('contenu', 'contenu :', ['type' => 'textarea']); ?>
 	<button class="btn btn-primary">Sauvegarder</button>
 </form>
+<br>
