198e6de8baf2c6415c1eb217f7fec470b3f369a7
Hollebeque Fabien <hollebeque.fabien@hotmail.com>
2017-11-02 13:18:01 +0100

    Rectification on line 2

--- app/Views/admin/commentaire/Add.php
+++ app/Views/admin/commentaire/Add.php
@@ -1,5 +1,5 @@
 <form method="post">
-	<?= $form->input('idArticle', 'AdArticle'); ?>
+	<?= $form->input('idArticle', 'IdArticle'); ?>
 	<?= $form->input('auteur', 'nom l\'auteur'); ?>
 	<?= $form->input('contenu', 'contenu', ['type' => 'textarea']); ?>
 	<button class="btn btn-primary">Sauvegarder</button>
