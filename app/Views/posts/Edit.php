f4e1fe198ffb958cd48d48b55743cab94ea00884
Hollebeque Fabien <hollebeque.fabien@hotmail.com>
2017-11-02 13:23:58 +0100

    Adding ':' for field names

--- app/Views/posts/Edit.php
+++ app/Views/posts/Edit.php
@@ -1,7 +1,8 @@
 <form method="post">
-	<?= $form->input('auteur', 'nom l\'auteur'); ?>
-	<?= $form->input('titre', 'titre de l\'article'); ?>
-	<?= $form->input('chapo', 'chapo', ['type' => 'textarea']); ?>
-	<?= $form->input('contenu', 'contenu', ['type' => 'textarea']); ?>
+	<?= $form->input('auteur', 'nom l\'auteur :'); ?>
+	<?= $form->input('titre', 'titre de l\'article :'); ?>
+	<?= $form->input('chapo', 'chapo :', ['type' => 'textarea']); ?>
+	<?= $form->input('contenu', 'contenu :', ['type' => 'textarea']); ?>
 	<button class="btn btn-primary">Sauvegarder</button>
 </form>
+<br />
