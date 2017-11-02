4e93e8df81e338443269cea40ab1296964d2b790
Hollebeque Fabien <hollebeque.fabien@hotmail.com>
2017-11-02 13:30:45 +0100

    Changing the icon, viewport, redirects and adding script

--- app/Views/templates/Default.php
+++ app/Views/templates/Default.php
@@ -4,10 +4,10 @@
   <head>
 
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="viewport" content="width=device-width, initial-scale=0.70, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-    <link rel="icon" href="../../../../favicon.ico">
+    <link rel="icon" type="image/png" href="images/logo.jpg">
 
     <title><?= App::getInstance()->title; ?></title>
 
@@ -16,7 +16,7 @@
 
     <!-- Custom styles for this template -->
     <link href="css/one-page-wonder.css" rel="stylesheet">
-    <link rel="stylesheet" href="css/Blog.css">
+    <link rel="stylesheet" href="css/blog.css">
 
   </head>
 
@@ -25,22 +25,22 @@
     <!-- Navigation -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
       <div class="container">
-        <a class="navbar-brand" href="Index.php"><?= App::getInstance()->title; ?></a>
+        <a class="navbar-brand" href="index.php"><?= App::getInstance()->title; ?></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarResponsive">
           <ul class="navbar-nav ml-auto">
             <li class="nav-item active">
-              <a class="nav-link" href="Index.php">Accueil
+              <a class="nav-link" href="index.php">Accueil
                 <span class="sr-only">(current)</span>
               </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="Index.php?p=posts.index">Blog</a>
+              <a class="nav-link" href="index.php?p=posts.index">Blog</a>
             </li>
               <li class="nav-item">
-              <a class="nav-link" href="Index.php?p=contact.index">Contact</a>
+              <a class="nav-link" href="index.php?p=contact.index">Contact</a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="CV/CV_Hollebeque_Fabien.pdf">CV</a>
@@ -59,7 +59,7 @@
       </div>
     </header>
 
-        <p class="img-logo"><img class="img-logo-style img-fluid rounded-circle" src="images/logo.jpg"></p>
+       <p class="img-logo"><a href="index.php"><img class="img-logo-style img-fluid rounded-circle" src="images/logo.jpg"></a></p>
     
     <div class="container">
 
@@ -78,6 +78,11 @@
       </div>
       <!-- /.container -->
     </footer>
+
+        <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/popper/popper.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
 
   </body>
 </html>
