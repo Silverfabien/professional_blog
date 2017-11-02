5c0ec156a5eb34f2571b420d5b13b56ec74e4099
Hollebeque Fabien <hollebeque.fabien@hotmail.com>
2017-11-02 13:13:13 +0100

    Deleting the function 'lastCommentaire'

--- app/Table/CommentaireTable.php
+++ app/Table/CommentaireTable.php
@@ -18,14 +18,8 @@ class CommentaireTable extends Table
 		return $this->query("SELECT commentaire.id, commentaire.contenu, commentaire.auteur, commentaire.date FROM commentaire ORDER BY commentaire.date DESC");
 	}
 
-	public function lastCommentaire($id)
-	{
-		return $this->query("SELECT commentaire.id, commentaire.auteur, commentaire.contenu, commentaire.date FROM commentaire WHERE commentaire.id = ?", [$id], true);
-	}
-
 	public function lastCommentaires($idArticle)
 	{
 		return $this->query("SELECT id, auteur, contenu, `date` FROM commentaire WHERE idArticle = ?", [$idArticle]);
 	}
-
 }
