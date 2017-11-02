23acb5ba1672778a16e80e30c40f63d795c6ecab
Hollebeque Fabien <hollebeque.fabien@hotmail.com>
2017-11-02 13:11:15 +0100

    Change the location and modification of 'setlocale'

--- app/Entity/PostEntity.php
+++ app/Entity/PostEntity.php
@@ -8,7 +8,7 @@ class PostEntity extends Entity
 {
 	public function getUrl()
 	{
-		return 'Index.php?p=posts.show&id=' . $this->id;
+		return 'index.php?p=posts.show&id=' . $this->id;
 	}
 
 	public function getExtrait()
@@ -21,8 +21,8 @@ class PostEntity extends Entity
 	public function getDateformat()
 	{
 		$a = strtotime($this->dateAjout);
-		setlocale(LC_ALL, 'FR').': ';
-		$date = iconv('ISO-8859-1', 'UTF-8', strftime('%A %d %B %Y', $a) . ' a ' . strftime('%H', $a) . 'h' . strftime('%M', $a) . '<br />');
+		setlocale(LC_TIME, 'fr_FR.utf8','fra').': ';
+		$date = iconv('ISO-8859-1', 'UTF-8', strftime('%A %d %B %Y', $a) . ' a ' . strftime('%H', $a) . 'h' . strftime('%M', $a));
 		return $date;
 	}
 }
