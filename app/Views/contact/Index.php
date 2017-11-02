9c7770bd6c0ca18b90a14df9cb1e06729026dcb0
Hollebeque Fabien <hollebeque.fabien@hotmail.com>
2017-11-02 13:22:06 +0100

    Adding the contact part

--- app/Views/contact/Index.php
+++ app/Views/contact/Index.php
@@ -1,2 +1,34 @@
-<?php var_dump($form); ?>
-<?= $form->input('auteur', 'auteur'); ?>
+<?php session_start() ?>
+
+<h2>Me contacter</h2><br>
+
+<p>Vous voulez me contacter pour plus d'informations ? Alors remplissez les champs ci-dessous et je vous répondrai dans les plus brefs délais.</p>
+
+<?php if(array_key_exists('errors', $_SESSION)): ?>
+
+	<div class="alert alert-danger">
+		<?= implode('<br>', $_SESSION['errors']); ?>
+	</div>
+<?php endif; ?>
+
+<?php if(array_key_exists('success', $_SESSION)): ?>
+
+	<div class="alert alert-success">
+		Votre email a bien été envoyé.
+	</div>
+<?php endif; ?>
+
+<form method="post">
+	<?= $form->input('email', 'Votre email :'); ?>
+	<?= $form->input('sujet', 'Sujet :'); ?>
+	<?= $form->input('contenu', 'Contenu :'); ?>
+	<button class="btn btn-primary">Envoyer</button>
+</form>
+<br>
+
+<?php
+
+unset($_SESSION['errors']);
+unset($_SESSION['success']);
+
+?>
