a5884a2d441080780d94694fe7bcbbd4084f4d27
Hollebeque Fabien <hollebeque.fabien@hotmail.com>
2017-11-02 13:10:06 +0100

    modification of 'setlocale'

--- app/Entity/CommentaireEntity.php
+++ app/Entity/CommentaireEntity.php
@@ -6,15 +6,10 @@ use Core\Entity\Entity;
 
 class CommentaireEntity extends Entity
 {
-	public function getUrl()
-	{
-		return 'Index.php?p=commentaire.show&id=' . $this->id;
-	}
-
 	public function getDateformat()
 	{
 		$a = strtotime($this->date);
-		setlocale(LC_ALL, 'FR').': ';
+		setlocale(LC_TIME, 'fr_FR.utf8','fra').': ';
 		$date = iconv('ISO-8859-1', 'UTF-8', strftime('%A %d %B %Y', $a) . ' a ' . strftime('%H', $a) . 'h' . strftime('%M', $a));
 		return $date;
 	}
