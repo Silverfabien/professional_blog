45bd6ad6280b7c3969a85a2ab9d416286a8bb8de
Hollebeque Fabien <hollebeque.fabien@hotmail.com>
2017-11-02 13:06:16 +0100

    Deleting function 'add' and adding the management of the contact part

--- app/Controller/ContactController.php
+++ app/Controller/ContactController.php
@@ -18,20 +18,36 @@ class ContactController extends AppController
 		$form = new BootstrapForm($_POST);
 		$this->render('contact.index', compact('form'));
 	}
+}
 
-	public function add()
-	{
-		if(!empty($_POST))
-		{
-			$result = $this->Contact->create(['auteur' => $_POST['auteur'], 'email' => $_POST['email'], 'sujet' => $_POST['sujet'], 'contenu' => $_POST['contenu']]);
-			if($result)
-			{
-				return $this->index();
-			}
-		}
-		$form = new BootstrapForm($_POST);
-		$this->render('contact.index', compact('form'));
-	}
+$errors = [];
+
+if(!array_key_exists('sujet', $_POST) || $_POST['sujet'] == '')
+{
+	$errors['sujet'] = "Vous n'avez pas renseigné le sujet";
+}
+if(!array_key_exists('email', $_POST) || $_POST['email'] == '' || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL))
+{
+	$errors['email'] = "Vous n'avez pas renseigné un email valide";
+}
+if(!array_key_exists('contenu', $_POST) || $_POST['contenu'] == '')
+{
+	$errors['contenu'] = "Vous n'avez pas renseigné le contenu";
+}
+
+session_start();
+
+if(!empty($errors))
+{
+	$_SESSION['errors'] = $errors;
+}
+else
+{
+	$_SESSION['success'] = 1;
+	$sujet = $_POST['sujet'];
+	$contenu = $_POST['contenu'];
+	$headers = 'FROM:' . $_POST['email'];
+	mail('silversat60@gmail.com', $sujet, $contenu, $headers);
 }
 
 ?>
