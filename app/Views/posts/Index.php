aed0178e969ff7ab5fc5d65779a5b289d430d228
Hollebeque Fabien <hollebeque.fabien@hotmail.com>
2017-11-10 13:56:13 +0100

    Complete

--- app/Views/posts/Index.php
+++ app/Views/posts/Index.php
@@ -1,15 +1,21 @@
-<p>
-	<a href="?p=posts.add" class="btn btn-success">Ajouter</a>
-</p>
-<div class="row">
-	<div class="col-sm-8">
-		
-		<?php foreach($posts as $post): ?>
+	<p>
+		<a href="?p=posts.add" class="btn btn-success">Ajouter</a>
+	</p>
+	<div class="row">
+		<div class="col-sm-12">
+			<?php foreach($posts as $post): ?>
 
-			<h2><a href="<?= $post->url ?>"><?= htmlspecialchars($post->titre); ?></a></h2>
+				<div style="border: 2px solid #bbbbbb; background: #eeeeee;">
+					<div style="background: #ddd; border-bottom: 1px solid #bbbbbb;">
+						<h2><a href="<?= $post->url ?>"><?= htmlspecialchars($post->titre); ?></a></h2>
 
-			<p><strong><?= htmlspecialchars($post->auteur); ?></strong> a posté le post le <?= $post->dateFormat; ?></p>
+						<p><em><strong><?= htmlspecialchars($post->auteur); ?></strong> a posté le post le <?= $post->dateFormat; ?></em></p>
+					</div>
+						<?= $post->extrait; ?>
+				</div>
+				<br>
 
-			<p><?= $post->extrait; ?></p>
+			<?php endforeach; ?>
 
-		<?php endforeach; ?>
+		</div>
+	</div>
